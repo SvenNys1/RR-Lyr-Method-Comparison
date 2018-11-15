5f00bf9e2f87a18bb74962a1bc1bc456d623d61e
JVB11 <45052356+JVB11@users.noreply.github.com>
2018-11-16 00:55:12 +0100

    License updated

--- Passing_Bablok.py
+++ Passing_Bablok.py
@@ -12,6 +12,19 @@
 # in Clinical Chemistry, Part I. Clinical Chemistry and Laboratory Medicine, 21(11), pp. 709-720. 
 # doi:10.1515/cclm.1983.21.11.709
 
+#   Copyright 2018 Jordan Van Beeck
+
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+
+#       http://www.apache.org/licenses/LICENSE-2.0
+
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 
 import numpy as np
 import matplotlib.pyplot as plt
