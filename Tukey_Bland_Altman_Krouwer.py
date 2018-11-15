9367adc38b13abd2204688a9af91a453fcb666fc
JVB11 <45052356+JVB11@users.noreply.github.com>
2018-11-16 00:55:43 +0100

    License updated

--- Tukey_Bland_Altman_Krouwer.py
+++ Tukey_Bland_Altman_Krouwer.py
@@ -7,6 +7,20 @@
 # Description: Module that implements the Tukey's mean difference plots/Bland-Altman plots/Krouwer plots 
 # for the statistical comparison of the agreement of two methods.
 
+#   Copyright 2018 Jordan Van Beeck
+
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+
+#       http://www.apache.org/licenses/LICENSE-2.0
+
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 import numpy as np
 import re
 import matplotlib.pyplot as plt
